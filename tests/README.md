d487aad5e703fceac729de8e611717306a5e03b7
DogeWatch <DogeWatch@users.noreply.github.com>
2020-06-18 18:27:46 +0800

    feat: add test (#25)
    * feat: add tests (#14)
    
    * feat: add tests
    
    * feat: change way of importing BinaryAIException
    
    Co-authored-by: dodgetang <dodgetang@tencent.com>
    
    * add test data and workflow (#17)
    
    * feat: add testdata
    
    * feat: add test workflow and README
    
    * feat: use pip install -r tests/requirements.txt in workflow
    
    * docs: update README.md in tests
    
    * update testdata (#24)
    
    * feat: add testdata
    
    * feat: add test workflow and README
    
    * feat: use pip install -r tests/requirements.txt in workflow
    
    * docs: update README.md in tests
    
    * feat: update testdata
    
    * docs: update test README.md
    
    Co-authored-by: dodgetang <dodgetang@tencent.com>

---
+++ tests/README.md
@@ -0,0 +1,18 @@
+# binaryai test
+
+## requirement
+```shell
+$ pip install -r requirements.txt
+```
+
+## usage
+
+test function module
+```shell
+$ pytest -v -n auto test_function --url {API_URL} --token {TOKEN}
+```
+
+test command line
+```shell
+$ pytest -v -n auto test_cli --url {API_URL} --token {TOKEN}
+```
---
+++ tests/test_function/conftest.py
@@ -0,0 +1,26 @@
+import os
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption("--url", action="store", default="https://api.binaryai.tencent.com/v1/endpoint")
+    parser.addoption("--token", action="store")
+
+
+@pytest.fixture(scope="module")
+def client(request):
+    from binaryai.client import Client
+    url = request.config.getoption("--url")
+    token = request.config.getoption("--token")
+    return Client(url=url, token=token)
+
+
+@pytest.fixture(scope="module")
+def data_1():
+    import pandas
+    return pandas.read_pickle("{}/../testdata/test.pkl".format(os.path.dirname(__file__)))
+
+@pytest.fixture(scope="module")
+def data_2():
+    import pandas
+    return pandas.read_pickle("{}/../testdata/test_official.pkl".format(os.path.dirname(__file__)))
