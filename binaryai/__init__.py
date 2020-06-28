48ce920a29652bf60c2491ef14dd9497159083d1
ltchen <ltchen@tencent.com>
2020-06-28 17:34:02 +0800

    feat: add version check

--- binaryai/__init__.py
+++ binaryai/__init__.py
@@ -1,5 +1,7 @@
 from binaryai import client, function
 from .error import BinaryAIException
+import requests
+import json
 
 
 def _get_version(default='version not found'):
@@ -14,7 +16,23 @@ def _get_version(default='version not found'):
             return default
 
 
+def _get_latest_version():
+    try:
+        resp = requests.get('https://pypi.org/pypi/binaryai/json')
+        info = resp.content.decode()
+        info = json.loads(info)
+        return info["info"]["version"]
+    except Exception as e:
+        raise BinaryAIException("SDK_ERROR", "[BinaryAI] Check version fail.")
+
+
 __version__ = _get_version()
+
+_latest_version = _get_latest_version()
+if not __version__.startswith(_latest_version):
+    raise BinaryAIException("SDK_ERROR",
+                            "[BinaryAI] Current version is {}, but the latest version is {}."
+                            .format(__version__, _latest_version))
 
 __all__ = [
     'client',
