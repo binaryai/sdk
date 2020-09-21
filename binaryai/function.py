e5f7879496f34e16a108a76a88245361c1a894e3
Daven Du <davendu@tencent.com>
2020-09-21 15:45:46 +0800

    Add additional fields for function insert (#93)
    * fix: add source code field
    
    * feat: add package name field

--- binaryai/function.py
+++ binaryai/function.py
@@ -16,6 +16,8 @@ def upload_function(
         binary_file=None,
         platform=None,
         throw_duplicate_error=False,
+        pseudo_code=None,
+        package_name=None
 ):
     '''
     upload function to BinaryAI server
@@ -30,6 +32,8 @@ def upload_function(
         binary_file(string): Name of the binary file which contains this function
         platform(string): Platform of the binary file, for example, metapc64, or x86_64, or mipsel
         throw_duplicate_error(bool): If a duplicate error should be raised when two name equals
+        pseudo_code(string): Pseudo code of the function
+        package_name(string): Package name containing this function
 
     Returns:
         * **id** (string) -- id of this function
@@ -43,7 +47,9 @@ def upload_function(
         'sourceFile': source_file,
         'sourceLine': source_line,
         'binaryFileName': binary_file,
-        'platform': platform
+        'platform': platform,
+        'pseudoCode': pseudo_code,
+        'packageName': package_name
     }
     r = client.execute(q_create_function, var, throw_duplicate_error=throw_duplicate_error)
     return r['createFunction']['function']['id']
